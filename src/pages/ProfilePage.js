c97951324dfc1b4f2959fa0938428677bb48a135
misheck12 <misheck1720@gmail.com>
2022-05-13 00:58:50 +0200

    Display joined missions on profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -4,9 +4,23 @@ import { useSelector } from 'react-redux';
 const ProfilePage = () => {
   const rockets = useSelector((state) => state.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  const missions = useSelector((state) => state.missions);
+  const reservedMissions = missions.filter((mission) => mission.join === true);
 
   return (
     <div className="profile">
+      <div>
+        <h2>My Missions</h2>
+        {reservedMissions.length === 0 ? (
+          <p className="empty-profile">No Misson joined Yet!</p>
+        ) : (
+          reservedMissions.map((mission) => (
+            <p key={mission.mission_id} className="profile-missions">
+              {mission.mission_name}
+            </p>
+          ))
+        )}
+      </div>
       <div>
         <h2>My Rockets</h2>
         {reservedRockets.length === 0 ? (
