95d7e9cc44df315c4d6bb4f73a4bf9e6c1c60cc5
Munezero Christian <chamsf5@aol.com>
2022-05-13 00:48:11 +0200

    Display reserved rockets inside my profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const ProfilePage = () => <div>ProfilePage</div>;
+const ProfilePage = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+
+  return (
+    <div className="profile">
+      <div>
+        <h2>My Rockets</h2>
+        {reservedRockets.length === 0 ? (
+          <p className="empty-profile">No Reserved Rocket Yet!</p>
+        ) : (
+          reservedRockets.map((rocket) => (
+            <p key={rocket.rocket_id} className="profile-rockets">
+              {rocket.rocket_name}
+            </p>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default ProfilePage;
